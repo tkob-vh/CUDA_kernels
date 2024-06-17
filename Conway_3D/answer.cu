720157657d7892781109395d05b9d9022c903240
yyx <yuxin.yan.cs@gmail.com>
2024-06-17 15:03:48 +0800

    feat: change the naive version to improve memory throughput

--- Conway_3D/answer.cu
+++ Conway_3D/answer.cu
@@ -8,39 +8,44 @@
 #include <chrono>
 #include <cuda_runtime.h>
 
+#define BLOCK_WIDTH 32
+#define Z_ITER 32
 namespace fs = std::filesystem;
 
 __global__ void conway_step(uint8_t *curr_space, uint8_t *next_space, size_t M) {
-  int x = blockDim.x * blockIdx.x + threadIdx.x;
-  int y = blockDim.y * blockIdx.y + threadIdx.y;
-  int z = blockDim.z * blockIdx.z + threadIdx.z;
 
-  int lx = (x + M - 1) % M * M * M;
-  int mx = x * M * M;
-  int rx = (x + 1) % M * M * M;
-  int ly = (y + M - 1) % M * M;
-  int my = y * M;
-  int ry = (y + 1) % M * M;
-  int lz = (z + M - 1) % M;
-  int mz = z;
-  int rz = (z + 1) % M;
+  int i = blockDim.z * blockIdx.z + threadIdx.z;
+  int j = blockDim.y * blockIdx.y + threadIdx.y;
+  int k = blockDim.x * blockIdx.x + threadIdx.x;
 
-  uint8_t curr_state = curr_space[mx + my + mz];
-  uint8_t &next_state = next_space[mx + my + mz];
+  int li = (i + M - 1) % M * M * M;
+  int mi = i * M * M;
+  int ri = (i + 1) % M * M * M;
 
-  uint8_t neighbor_count = curr_space[lx + ly + lz] + curr_space[lx + ly + mz] +
-                          curr_space[lx + ly + rz] + curr_space[lx + my + lz] +
-                          curr_space[lx + my + mz] + curr_space[lx + my + rz] +
-                          curr_space[lx + ry + lz] + curr_space[lx + ry + mz] +
-                          curr_space[lx + ry + rz] + curr_space[mx + ly + lz] +
-                          curr_space[mx + ly + mz] + curr_space[mx + ly + rz] +
-                          curr_space[mx + my + lz] + curr_space[mx + my + rz] +
-                          curr_space[mx + ry + lz] + curr_space[mx + ry + mz] +
-                          curr_space[mx + ry + rz] + curr_space[rx + ly + lz] +
-                          curr_space[rx + ly + mz] + curr_space[rx + ly + rz] +
-                          curr_space[rx + my + lz] + curr_space[rx + my + mz] +
-                          curr_space[rx + my + rz] + curr_space[rx + ry + lz] +
-                          curr_space[rx + ry + mz] + curr_space[rx + ry + rz];
+  int lj = (j + M - 1) % M * M;
+  int mj = j * M;
+  int rj = (j + 1) % M * M;
+
+  int lk = (k + M - 1) % M;
+  int mk = k;
+  int rk = (k + 1) % M;
+
+  uint8_t curr_state = curr_space[mi + mj + mk];
+  uint8_t &next_state = next_space[mi + mj + mk];
+
+  uint8_t neighbor_count = curr_space[li + lj + lk] + curr_space[li + lj + mk] +
+                          curr_space[li + lj + rk] + curr_space[li + mj + lk] +
+                          curr_space[li + mj + mk] + curr_space[li + mj + rk] +
+                          curr_space[li + rj + lk] + curr_space[li + rj + mk] +
+                          curr_space[li + rj + rk] + curr_space[mi + lj + lk] +
+                          curr_space[mi + lj + mk] + curr_space[mi + lj + rk] +
+                          curr_space[mi + mj + lk] + curr_space[mi + mj + rk] +
+                          curr_space[mi + rj + lk] + curr_space[mi + rj + mk] +
+                          curr_space[mi + rj + rk] + curr_space[ri + lj + lk] +
+                          curr_space[ri + lj + mk] + curr_space[ri + lj + rk] +
+                          curr_space[ri + mj + lk] + curr_space[ri + mj + mk] +
+                          curr_space[ri + mj + rk] + curr_space[ri + rj + lk] +
+                          curr_space[ri + rj + mk] + curr_space[ri + rj + rk];
 
   if(curr_state == 1) {
     if(neighbor_count < 5 || neighbor_count > 7) next_state = 0;
@@ -50,6 +55,7 @@ __global__ void conway_step(uint8_t *curr_space, uint8_t *next_space, size_t M) 
     if(neighbor_count == 6) next_state = 1;
     else next_state = 0;
   }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -58,7 +64,6 @@ int main(int argc, char *argv[]) {
               << std::endl;
     return 1;
   }
-  auto t1 = std::chrono::steady_clock::now();
 
   fs::path input_path = argv[1];
   fs::path output_path = argv[2];
@@ -85,17 +90,25 @@ int main(int argc, char *argv[]) {
 
   dim3 blockDim(16, 8, 8);
   dim3 gridDim(ceil((float) M / blockDim.x), ceil((float) M / blockDim.y), ceil((float) M / blockDim.z));
+  // dim3 blockDim(BLOCK_WIDTH, BLOCK_WIDTH, 1);
+  // dim3 gridDim(ceil((float) M / blockDim.x), ceil((float) M / blockDim.y), ceil((float) M / Z_ITER));
+
+
+  auto t1 = std::chrono::steady_clock::now();
 
   for(int i = 0; i < N; i++){
     conway_step<<<gridDim, blockDim>>>(curr_space_d, next_space_d, M);
+    #ifdef DEBUG
     cudaError_t error = cudaGetLastError();
     if(error != cudaSuccess) {
       std::cerr << "CUDA kernel launched failed: " << cudaGetErrorString(error) << std::endl;
     }
+    #endif
     cudaDeviceSynchronize();
     std::swap(curr_space_d, next_space_d);
   }
 
+  auto t2 = std::chrono::steady_clock::now();
   cudaMemcpy(curr_space, curr_space_d, M3 * sizeof(uint8_t), cudaMemcpyDeviceToHost);
 
   T += N;
@@ -110,7 +123,6 @@ int main(int argc, char *argv[]) {
   cudaFree(curr_space_d);
   cudaFree(next_space_d);
 
-  auto t2 = std::chrono::steady_clock::now();
   int duration = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count();
   std::cout << duration << std::endl;
 
