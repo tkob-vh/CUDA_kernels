2242691ce268a0078921501e9a3f5dfbdec904f0
yyx <yuxin.yan.cs@gmail.com>
2024-06-27 22:12:27 +0800

    feat: implement 1D shared memory, with some bugs

--- Conway_3D/answer.cu
+++ Conway_3D/answer.cu
@@ -8,71 +8,89 @@
 #include <chrono>
 #include <cuda_runtime.h>
 
-#define BLOCK_WIDTH 16
-#define INPUT_TILE BLOCK_WIDTH
-#define OUTPUT_TILE 14
-#define Z_ITER 14
+#define Z_WIDTH 4
 
 namespace fs = std::filesystem;
 
-__global__ void conway_step(uint8_t *curr_space, uint8_t *next_space, size_t M) {
-  __shared__ uint8_t curr_space_fro_s[INPUT_TILE][INPUT_TILE];
-  __shared__ uint8_t curr_space_mid_s[INPUT_TILE][INPUT_TILE];
-  __shared__ uint8_t curr_space_end_s[INPUT_TILE][INPUT_TILE];
 
+__global__ void conway_step(uint8_t *curr_space, uint8_t *next_space, size_t width, size_t M) {
+  extern __shared__ char inputs[];
+  uint32_t *src = (uint32_t *)inputs; // One uint32_t stores 4 cells in the x axis.
+  uint32_t *input = (uint32_t *)(inputs + width * Z_WIDTH * sizeof(uint32_t));
+  uint8_t *src_ = (uint8_t*)src;
+
+  int y = blockIdx.y;
+  int zStart = blockIdx.z; // One thread is responsible for Z_WIDTH cells starting at zStart in the z axis.
   int tx = threadIdx.x;
-  int ty = threadIdx.y;
-  
 
-  int iStart = Z_ITER * blockIdx.z; // the start index of the output_tile in z axis.
-  int j = OUTPUT_TILE * blockIdx.y + ty - 1;
-  int k = OUTPUT_TILE * blockIdx.x + tx - 1;
+  // Calculate the y index needed for the 4 cells in one thread.
+  int y_index[3];
+  y_index[0] = (y + M - 1) % M;
+  y_index[1] = y;
+  y_index[2] = (y + 1) % M;
 
-  int mj_k = ((j + M) % M) * M + (k + M) % M; 
-  int M2 = M * M;
+  uint32_t count[Z_WIDTH + 2]; // Store the number of alive cells in the 3 * 4 cells near the target cell.
 
-  curr_space_fro_s[ty][tx] = curr_space[((iStart + M - 1) % M) * M2 + mj_k];
-  curr_space_mid_s[ty][tx] = curr_space[iStart * M2 + mj_k];
+  for(int i = 0; i < Z_WIDTH + 2; i++) { // Iterate through z axis.
+    int zCurr = (zStart * Z_WIDTH + i - 1 + M) % M;
 
-  for(int i = iStart; i < iStart + Z_ITER; i++) {
-    curr_space_end_s[ty][tx] = curr_space[((i + 1) % M) * M2 + mj_k];
+    count[i] = 0;
+    for(int j = 0; j < 3; j++) { // Iterate through y axis.
+      count[i] += curr_space[(zCurr * M + y_index[j]) * width + tx];
+    }
+  }
 
-    __syncthreads();
+  // Load the input cell into shared memory
+  for(int i = 0; i < Z_WIDTH; i++) {
+    src[i * width + tx] = curr_space[(zStart * Z_WIDTH + i) * width * M + y * width + tx];
+  }
 
-    if((unsigned int)(i - 0) < M && (unsigned int)(j - 0) < M && (unsigned int)(k - 0) < M && 
-       (unsigned int)(tx - 1) < INPUT_TILE - 2 && (unsigned int)(ty - 1) < INPUT_TILE - 2 ) {
-        uint8_t neighbor_count = curr_space_mid_s[ty - 1][tx - 1] + curr_space_mid_s[ty - 1][tx]
-                               + curr_space_mid_s[ty - 1][tx + 1] + curr_space_mid_s[ty][tx - 1]
-                               + curr_space_mid_s[ty][tx + 1] + curr_space_mid_s[ty + 1][tx - 1]
-                               + curr_space_mid_s[ty + 1][tx] + curr_space_mid_s[ty + 1][tx + 1]
-                               + curr_space_fro_s[ty - 1][tx - 1] + curr_space_fro_s[ty - 1][tx]
-                               + curr_space_fro_s[ty - 1][tx + 1] + curr_space_fro_s[ty][tx - 1]
-                               + curr_space_fro_s[ty][tx] + curr_space_fro_s[ty][tx + 1]
-                               + curr_space_fro_s[ty + 1][tx - 1] + curr_space_fro_s[ty + 1][tx]
-                               + curr_space_fro_s[ty + 1][tx + 1] + curr_space_end_s[ty - 1][tx - 1]
-                               + curr_space_end_s[ty - 1][tx] + curr_space_end_s[ty - 1][tx + 1]
-                               + curr_space_end_s[ty][tx - 1] + curr_space_end_s[ty][tx]
-                               + curr_space_end_s[ty][tx + 1] + curr_space_end_s[ty + 1][tx - 1]
-                               + curr_space_end_s[ty + 1][tx] + curr_space_end_s[ty + 1][tx + 1];
+  __syncthreads();
+  // Calculate the number of alive cells across 3 z indexs. Finally we get the number of alive cells for each Z_WIDTH cell without considering its x-axis neighbors.
+  for(int i = 0; i < Z_WIDTH; i++) {
+    count[i] = count[i] + count[i + 1] + count[i + 2];
+  }
 
-        uint8_t curr_state = curr_space_mid_s[ty][tx];
-        uint8_t &next_state = next_space[i * M2 + mj_k];  // Corrected to use correct indexing                        
+  // Load the count array from local memory to shared memory.
+  for(int i = 0; i < Z_WIDTH; i++) {
+    input[i * (width + 2) + tx + 1] = count[i];
+  }
 
-        if(curr_state == 1) {
-          if(neighbor_count < 5 || neighbor_count > 7) next_state = 0;
-          else next_state = 1;
-        } else {
-          if(neighbor_count == 6) next_state = 1;
-          else next_state = 0;
-        }
+  __syncthreads();
 
+  // Load the count array for the cells in the near the halo.
+  if(tx < Z_WIDTH) {
+    input[tx * (width + 2)] = input[tx * (width + 2) + width];
+    input[tx * (width + 2) + (width + 1)] = input[tx * (width + 2) + 1];
+  }
+
+  __syncthreads();
+
+  uint32_t tempout[4];
+  for(int i = 0; i < Z_WIDTH; i++) {
+    // 
+    uint32_t left = input[i * (width + 2) + tx];
+    uint32_t center = input[i * (width + 2) + tx + 1];
+    uint32_t right = input[i * (width + 2) + tx + 2];
+    center = center + (center >> 8) + (right << 24) + (center << 8) + (left >> 24);
+
+    for(int j = 0; j < 4; j++) {
+      uint32_t c = center & 0xff;
+      center = center >> 8;
+
+      if(c == 6 || 6 <= c && c <= 8 && src_[(i * width + tx) * 4 + j]) {
+        tempout[j] = 1;
+      }
+      else {
+        tempout[j] = 0;
+      }
     }
 
-    __syncthreads();
+    uint32_t out = tempout[0] + (tempout[1] << 8) + (tempout[2] << 16) + (tempout[3] << 24);
+    next_space[((zStart * Z_WIDTH + i) * M + y) * width + tx] = out;
 
-    curr_space_fro_s[ty][tx] = curr_space_mid_s[ty][tx];
-    curr_space_mid_s[ty][tx] = curr_space_end_s[ty][tx];
   }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -100,28 +118,51 @@ int main(int argc, char *argv[]) {
 
   input_file.read(reinterpret_cast<char *>(curr_space), M3);
 
+  cudaError_t err;
   uint8_t *curr_space_d, *next_space_d;
-  cudaMalloc(&curr_space_d, M3 * sizeof(uint8_t));
-  cudaMalloc(&next_space_d, M3 * sizeof(uint8_t));
-  cudaMemcpy(curr_space_d, curr_space, M3 * sizeof(uint8_t), cudaMemcpyHostToDevice);
-
-  dim3 blockDim(BLOCK_WIDTH, BLOCK_WIDTH, 1);
-  dim3 gridDim(ceil((float) M / OUTPUT_TILE), ceil((float) M / OUTPUT_TILE), ceil((float) M / Z_ITER));
+  err = cudaMalloc(&curr_space_d, M3 * sizeof(uint8_t));
+  if(err != cudaSuccess) {
+    std::cerr << cudaGetErrorString(err) << std::endl;
+  }
+  err = cudaMalloc(&next_space_d, M3 * sizeof(uint8_t));
+  if(err != cudaSuccess) {
+    std::cerr << cudaGetErrorString(err) << std::endl;
+  }
+  err = cudaMemcpy(curr_space_d, curr_space, M3 * sizeof(uint8_t), cudaMemcpyHostToDevice);
+  if(err != cudaSuccess) {
+    std::cerr << cudaGetErrorString(err) << std::endl;
+  }
 
   auto t1 = std::chrono::steady_clock::now();
 
+  size_t width = M / 4; 
+  dim3 blockDim(width, 1, 1);
+  dim3 gridDim(1, M, M / Z_WIDTH);
+
+  size_t shared_region = width *  Z_WIDTH * sizeof(uint32_t) + (width + 2) * (Z_WIDTH + 2) * sizeof(uint32_t);
+
   for(int i = 0; i < N / 2; i++) {
-    conway_step<<<gridDim, blockDim>>>(curr_space_d, next_space_d, M);
-    conway_step<<<gridDim, blockDim>>>(next_space_d, curr_space_d, M);
-    cudaError_t error = cudaGetLastError();
-    if(error != cudaSuccess) {
-      std::cerr << "CUDA kernel launched failed: " << cudaGetErrorString(error) << std::endl;
-    }
+    conway_step<<<gridDim, blockDim, shared_region>>>(curr_space_d, next_space_d, width, M);
+
+    conway_step<<<gridDim, blockDim, shared_region>>>(next_space_d, curr_space_d, width, M);
+  }
+  err = cudaGetLastError();
+  if(err != cudaSuccess) {
+    std::cerr << cudaGetErrorString(err) << std::endl;
+  }
+  
+
+  err = cudaDeviceSynchronize();
+  if(err != cudaSuccess) {
+    std::cerr << cudaGetErrorString(err) << std::endl;
   }
 
-  cudaDeviceSynchronize();
   auto t2 = std::chrono::steady_clock::now();
-  cudaMemcpy(curr_space, curr_space_d, M3 * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+
+  err = cudaMemcpy(curr_space, curr_space_d, M3 * sizeof(uint8_t), cudaMemcpyDeviceToHost);
+  if(err != cudaSuccess) {
+    std::cerr << cudaGetErrorString(err) << std::endl;
+  }
   
   T += N;
   std::ofstream output_file(output_path, std::ios::binary);
